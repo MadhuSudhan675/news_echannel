609bb6eae4a965c309d8b66d390d335e56814337
madhusudhan <chmadhu675@gmail.com>
2024-05-07 23:39:42 +0530

    card images arranged randomly

--- navAnchorPage.js
+++ navAnchorPage.js
@@ -1,109 +1,325 @@
 // Array of card data
 var tsCardData = [
-  { heading: "May 15 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 14 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 13 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 12 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 11 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 10 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 09 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 08 2024", image: "./daily-news-pdf/webpimage.webp" },
+  {
+    heading: "May 15 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0008-min.webp",
+  },
+  {
+    heading: "May 14 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0007-min.webp",
+  },
+  {
+    heading: "May 13 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0006-min.webp",
+  },
+  {
+    heading: "May 12 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0005-min.webp",
+  },
+  {
+    heading: "May 11 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0004-min.webp",
+  },
+  {
+    heading: "May 10 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0003-min.webp",
+  },
+  {
+    heading: "May 09 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0002-min.webp",
+  },
+  {
+    heading: "May 08 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0001-min.webp",
+  },
   // Add more card data as needed
 ];
 
 var apCardData = [
-  { heading: "May 15 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 14 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 13 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 12 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 11 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 10 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 09 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 08 2024", image: "./daily-news-pdf/webpimage.webp" },
+  {
+    heading: "May 15 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0004-min.webp",
+  },
+  {
+    heading: "May 14 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0005-min.webp",
+  },
+  {
+    heading: "May 13 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0006-min.webp",
+  },
+  {
+    heading: "May 12 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0007-min.webp",
+  },
+  {
+    heading: "May 11 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0008-min.webp",
+  },
+  {
+    heading: "May 10 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0009-min.webp",
+  },
+  {
+    heading: "May 09 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0010-min.webp",
+  },
+  {
+    heading: "May 08 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0012-min.webp",
+  },
   // Add more card data as needed
 ];
 
 var mahabubnagarCardData = [
-  { heading: "May 15 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 14 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 13 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 12 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 11 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 10 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 09 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 08 2024", image: "./daily-news-pdf/webpimage.webp" },
+  {
+    heading: "May 15 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0012-min.webp",
+  },
+  {
+    heading: "May 14 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0011-min.webp",
+  },
+  {
+    heading: "May 13 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0010-min.webp",
+  },
+  {
+    heading: "May 12 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0009-min.webp",
+  },
+  {
+    heading: "May 11 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0008-min.webp",
+  },
+  {
+    heading: "May 10 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0007-min.webp",
+  },
+  {
+    heading: "May 09 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0006-min.webp",
+  },
+  {
+    heading: "May 08 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0005-min.webp",
+  },
   // Add more card data as needed
 ];
 
 var medakCardData = [
-  { heading: "May 15 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 14 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 13 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 12 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 11 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 10 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 09 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 08 2024", image: "./daily-news-pdf/webpimage.webp" },
+  {
+    heading: "May 15 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0002-min.webp",
+  },
+  {
+    heading: "May 14 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0003-min.webp",
+  },
+  {
+    heading: "May 13 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0004-min.webp",
+  },
+  {
+    heading: "May 12 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0005-min.webp",
+  },
+  {
+    heading: "May 11 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0006-min.webp",
+  },
+  {
+    heading: "May 10 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0007-min.webp",
+  },
+  {
+    heading: "May 09 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0008-min.webp",
+  },
+  {
+    heading: "May 08 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0009-min.webp",
+  },
   // Add more card data as needed
 ];
 
 var nalgondaCardData = [
-  { heading: "May 15 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 14 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 13 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 12 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 11 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 10 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 09 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 08 2024", image: "./daily-news-pdf/webpimage.webp" },
+  {
+    heading: "May 15 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0006-min.webp",
+  },
+  {
+    heading: "May 14 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0007-min.webp",
+  },
+  {
+    heading: "May 13 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0008-min.webp",
+  },
+  {
+    heading: "May 12 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0009-min.webp",
+  },
+  {
+    heading: "May 11 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0010-min.webp",
+  },
+  {
+    heading: "May 10 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0011-min.webp",
+  },
+  {
+    heading: "May 09 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0012-min.webp",
+  },
+  {
+    heading: "May 08 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0012-min.webp",
+  },
   // Add more card data as needed
 ];
 
 var nizamabadCardData = [
-  { heading: "May 15 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 14 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 13 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 12 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 11 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 10 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 09 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 08 2024", image: "./daily-news-pdf/webpimage.webp" },
+  {
+    heading: "May 15 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0011-min.webp",
+  },
+  {
+    heading: "May 14 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0010-min.webp",
+  },
+  {
+    heading: "May 13 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0009-min.webp",
+  },
+  {
+    heading: "May 12 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0008-min.webp",
+  },
+  {
+    heading: "May 11 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0007-min.webp",
+  },
+  {
+    heading: "May 10 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0006-min.webp",
+  },
+  {
+    heading: "May 09 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0005-min.webp",
+  },
+  {
+    heading: "May 08 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0004-min.webp",
+  },
   // Add more card data as needed
 ];
 
 var adilabadCardData = [
-  { heading: "May 15 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 14 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 13 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 12 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 11 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 10 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 09 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 08 2024", image: "./daily-news-pdf/webpimage.webp" },
+  {
+    heading: "May 15 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0004-min.webp",
+  },
+  {
+    heading: "May 14 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0005-min.webp",
+  },
+  {
+    heading: "May 13 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0006-min.webp",
+  },
+  {
+    heading: "May 12 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0007-min.webp",
+  },
+  {
+    heading: "May 11 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0008-min.webp",
+  },
+  {
+    heading: "May 10 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0009-min.webp",
+  },
+  {
+    heading: "May 09 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0004-min.webp",
+  },
+  {
+    heading: "May 08 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0006-min.webp",
+  },
   // Add more card data as needed
 ];
 
 var karimnagarCardData = [
-  { heading: "May 15 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 14 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 13 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 12 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 11 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 10 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 09 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 08 2024", image: "./daily-news-pdf/webpimage.webp" },
+  {
+    heading: "May 15 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0012-min.webp",
+  },
+  {
+    heading: "May 14 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0001-min.webp",
+  },
+  {
+    heading: "May 13 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0005-min.webp",
+  },
+  {
+    heading: "May 12 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0005-min.webp",
+  },
+  {
+    heading: "May 11 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0006-min.webp",
+  },
+  {
+    heading: "May 10 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0008-min.webp",
+  },
+  {
+    heading: "May 09 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0003-min.webp",
+  },
+  {
+    heading: "May 08 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0004-min.webp",
+  },
   // Add more card data as needed
 ];
 
 var warangalCardData = [
-  { heading: "May 15 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 14 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 13 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 12 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 11 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 10 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 09 2024", image: "./daily-news-pdf/webpimage.webp" },
-  { heading: "May 08 2024", image: "./daily-news-pdf/webpimage.webp" },
+  {
+    heading: "May 15 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0012-min.webp",
+  },
+  {
+    heading: "May 14 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0012-min.webp",
+  },
+  {
+    heading: "May 13 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0012-min.webp",
+  },
+  {
+    heading: "May 12 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0012-min.webp",
+  },
+  {
+    heading: "May 11 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0012-min.webp",
+  },
+  {
+    heading: "May 10 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0012-min.webp",
+  },
+  {
+    heading: "May 09 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0012-min.webp",
+  },
+  {
+    heading: "May 08 2024",
+    image: "./newsPageWebp/sample-news-pape_page-0012-min.webp",
+  },
   // Add more card data as needed
 ];
 
