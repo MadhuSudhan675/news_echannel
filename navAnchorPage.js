21ff32d622d513818730048efdf941ee8fa34b38
madhusudhan <chmadhu675@gmail.com>
2024-05-16 21:27:37 +0530

    all nav id's created and pdf displayed

--- navAnchorPage.js
+++ navAnchorPage.js
@@ -3,34 +3,42 @@ var tsCardData = [
   {
     heading: "May 15 2024",
     image: "./newsPageWebp/sample-news-pape_page-0008-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 14 2024",
     image: "./newsPageWebp/sample-news-pape_page-0007-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 13 2024",
     image: "./newsPageWebp/sample-news-pape_page-0006-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 12 2024",
     image: "./newsPageWebp/sample-news-pape_page-0005-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 11 2024",
     image: "./newsPageWebp/sample-news-pape_page-0004-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 10 2024",
     image: "./newsPageWebp/sample-news-pape_page-0003-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 09 2024",
     image: "./newsPageWebp/sample-news-pape_page-0002-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 08 2024",
     image: "./newsPageWebp/sample-news-pape_page-0001-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   // Add more card data as needed
 ];
@@ -39,34 +47,42 @@ var apCardData = [
   {
     heading: "May 15 2024",
     image: "./newsPageWebp/sample-news-pape_page-0004-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 14 2024",
     image: "./newsPageWebp/sample-news-pape_page-0005-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 13 2024",
     image: "./newsPageWebp/sample-news-pape_page-0006-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 12 2024",
     image: "./newsPageWebp/sample-news-pape_page-0007-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 11 2024",
     image: "./newsPageWebp/sample-news-pape_page-0008-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 10 2024",
     image: "./newsPageWebp/sample-news-pape_page-0009-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 09 2024",
     image: "./newsPageWebp/sample-news-pape_page-0010-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 08 2024",
     image: "./newsPageWebp/sample-news-pape_page-0012-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   // Add more card data as needed
 ];
@@ -75,34 +91,42 @@ var mahabubnagarCardData = [
   {
     heading: "May 15 2024",
     image: "./newsPageWebp/sample-news-pape_page-0012-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 14 2024",
     image: "./newsPageWebp/sample-news-pape_page-0011-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 13 2024",
     image: "./newsPageWebp/sample-news-pape_page-0010-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 12 2024",
     image: "./newsPageWebp/sample-news-pape_page-0009-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 11 2024",
     image: "./newsPageWebp/sample-news-pape_page-0008-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 10 2024",
     image: "./newsPageWebp/sample-news-pape_page-0007-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 09 2024",
     image: "./newsPageWebp/sample-news-pape_page-0006-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 08 2024",
     image: "./newsPageWebp/sample-news-pape_page-0005-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   // Add more card data as needed
 ];
@@ -111,34 +135,42 @@ var medakCardData = [
   {
     heading: "May 15 2024",
     image: "./newsPageWebp/sample-news-pape_page-0002-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 14 2024",
     image: "./newsPageWebp/sample-news-pape_page-0003-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 13 2024",
     image: "./newsPageWebp/sample-news-pape_page-0004-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 12 2024",
     image: "./newsPageWebp/sample-news-pape_page-0005-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 11 2024",
     image: "./newsPageWebp/sample-news-pape_page-0006-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 10 2024",
     image: "./newsPageWebp/sample-news-pape_page-0007-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 09 2024",
     image: "./newsPageWebp/sample-news-pape_page-0008-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 08 2024",
     image: "./newsPageWebp/sample-news-pape_page-0009-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   // Add more card data as needed
 ];
@@ -147,34 +179,42 @@ var nalgondaCardData = [
   {
     heading: "May 15 2024",
     image: "./newsPageWebp/sample-news-pape_page-0006-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 14 2024",
     image: "./newsPageWebp/sample-news-pape_page-0007-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 13 2024",
     image: "./newsPageWebp/sample-news-pape_page-0008-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 12 2024",
     image: "./newsPageWebp/sample-news-pape_page-0009-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 11 2024",
     image: "./newsPageWebp/sample-news-pape_page-0010-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 10 2024",
     image: "./newsPageWebp/sample-news-pape_page-0011-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 09 2024",
     image: "./newsPageWebp/sample-news-pape_page-0012-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 08 2024",
     image: "./newsPageWebp/sample-news-pape_page-0012-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   // Add more card data as needed
 ];
@@ -183,34 +223,42 @@ var nizamabadCardData = [
   {
     heading: "May 15 2024",
     image: "./newsPageWebp/sample-news-pape_page-0011-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 14 2024",
     image: "./newsPageWebp/sample-news-pape_page-0010-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 13 2024",
     image: "./newsPageWebp/sample-news-pape_page-0009-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 12 2024",
     image: "./newsPageWebp/sample-news-pape_page-0008-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 11 2024",
     image: "./newsPageWebp/sample-news-pape_page-0007-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 10 2024",
     image: "./newsPageWebp/sample-news-pape_page-0006-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 09 2024",
     image: "./newsPageWebp/sample-news-pape_page-0005-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 08 2024",
     image: "./newsPageWebp/sample-news-pape_page-0004-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   // Add more card data as needed
 ];
@@ -219,34 +267,42 @@ var adilabadCardData = [
   {
     heading: "May 15 2024",
     image: "./newsPageWebp/sample-news-pape_page-0004-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 14 2024",
     image: "./newsPageWebp/sample-news-pape_page-0005-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 13 2024",
     image: "./newsPageWebp/sample-news-pape_page-0006-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 12 2024",
     image: "./newsPageWebp/sample-news-pape_page-0007-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 11 2024",
     image: "./newsPageWebp/sample-news-pape_page-0008-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 10 2024",
     image: "./newsPageWebp/sample-news-pape_page-0009-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 09 2024",
     image: "./newsPageWebp/sample-news-pape_page-0004-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 08 2024",
     image: "./newsPageWebp/sample-news-pape_page-0006-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   // Add more card data as needed
 ];
@@ -255,34 +311,42 @@ var karimnagarCardData = [
   {
     heading: "May 15 2024",
     image: "./newsPageWebp/sample-news-pape_page-0012-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 14 2024",
     image: "./newsPageWebp/sample-news-pape_page-0001-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 13 2024",
     image: "./newsPageWebp/sample-news-pape_page-0005-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 12 2024",
     image: "./newsPageWebp/sample-news-pape_page-0005-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 11 2024",
     image: "./newsPageWebp/sample-news-pape_page-0006-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 10 2024",
     image: "./newsPageWebp/sample-news-pape_page-0008-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 09 2024",
     image: "./newsPageWebp/sample-news-pape_page-0003-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 08 2024",
     image: "./newsPageWebp/sample-news-pape_page-0004-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   // Add more card data as needed
 ];
@@ -291,42 +355,51 @@ var warangalCardData = [
   {
     heading: "May 15 2024",
     image: "./newsPageWebp/sample-news-pape_page-0001-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 14 2024",
     image: "./newsPageWebp/sample-news-pape_page-0012-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 13 2024",
     image: "./newsPageWebp/sample-news-pape_page-0005-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 12 2024",
     image: "./newsPageWebp/sample-news-pape_page-0011-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 11 2024",
     image: "./newsPageWebp/sample-news-pape_page-0012-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 10 2024",
     image: "./newsPageWebp/sample-news-pape_page-0006-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 09 2024",
     image: "./newsPageWebp/sample-news-pape_page-0012-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   {
     heading: "May 08 2024",
     image: "./newsPageWebp/sample-news-pape_page-0007-min.webp",
+    pdfFilePath: "./newsPageWebp/pidikili_pdf03-5-2024[1].pdf", // Add PDF file path here
   },
   // Add more card data as needed
 ];
 
 // Function to create a card element
-function createCard(heading, image) {
+function createCard(heading, image, id) {
   var cardDiv = document.createElement("div");
   cardDiv.classList.add("card");
+  cardDiv.id = id; // Assigning ID to the card
 
   var headingElem = document.createElement("h1");
   headingElem.classList.add("card-heading");
@@ -345,14 +418,20 @@ function createCard(heading, image) {
 
 // Function to add cards to the container
 function addCardsToContainer(container, data) {
-  data.forEach(function (item) {
-    console.log(item);
-    var card = createCard(item.heading, item.image);
+  data.forEach(function (item, index) {
+    // Using index to create unique IDs
+    var cardId = container.id + "-index-" + index; // Generating unique ID for each card
+    var card = createCard(item.heading, item.image, cardId);
     container.appendChild(card);
+    card.addEventListener("click", function () {
+      // Adding event listener to each card
+      addCardClickListener(cardId, "home-card-link", item.pdfFilePath); // Pass PDF file path as a parameter
+      console.log("Clicked card ID:", cardId); // Retrieving and logging the ID
+    });
   });
 }
 
-// Get the container element
+// Get the container elements
 var tsCardContainer = document.getElementById("nav-ts-card-container");
 var apCardContainer = document.getElementById("nav-ap-card-container");
 var mahabubnagarCardContainer = document.getElementById(
@@ -375,7 +454,7 @@ var warangalCardContainer = document.getElementById(
   "nav-warangal-card-container"
 );
 
-// Add cards to the container
+// Add cards to the containers
 addCardsToContainer(tsCardContainer, tsCardData);
 addCardsToContainer(apCardContainer, apCardData);
 addCardsToContainer(mahabubnagarCardContainer, mahabubnagarCardData);
